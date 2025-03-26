29754ae6df3ebcc138fe7ac4742800da2526ae2a
Germán Aliprandi <galiprandi@gmail.com>
2025-03-25 21:01:57 -0300

    Configurar GitHub Pages

--- src/settings.ts
+++ src/settings.ts
@@ -22,14 +22,14 @@ export const social = {
 }
 
 export const template = {
-	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+	website_url: 'https://galiprandi.github.io', // URL para GitHub Pages
 	menu_left: false,
 	transitions: true,
 	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
 	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
-    base: '' // Repository name starting with /
+    base: '/creandopuentes' // Nombre del repositorio comenzando con /
 }
 
 export const seo = {
